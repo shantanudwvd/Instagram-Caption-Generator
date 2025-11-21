f6e0fadde8feaa6fc1d70fd82ce279d4e5b7ce6b
Shantanu Dwivedi <shantanu.dwvd@gmail.com>
2025-11-21 17:14:52 +0530

    Add global scroll-to-top on route change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import './index.css';
 import ProfilePage from './pages/ProfilePage';
 import GeneratorPage from './pages/GeneratorPage';
@@ -10,12 +10,21 @@ import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import TipsPage from './pages/TipsPage';
 
+const ScrollToTop = () => {
+    const location = useLocation();
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: 'instant' });
+    }, [location.pathname]);
+    return null;
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <ThemeProvider>
             <AuthProvider>
                 <BrowserRouter>
+                    <ScrollToTop />
                     <Routes>
                         <Route path="/" element={<ProfilePage />} />
                         <Route path="/generator" element={<GeneratorPage />} />
